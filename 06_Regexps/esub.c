208a4da2ef7039b4d9ec33d4d2df452d52d3619a
Max Doledenok <doledenok@ispras.ru>
2022-12-24 16:45:21 +0300

    finish 06 task

--- 06_Regexps/esub.c
+++ 06_Regexps/esub.c
@@ -1,6 +1,6 @@
 #include <regex.h>
 #include <stdio.h>
-//#include <stdlib.h>
+#include <string.h>
 
 #define MATCH_COUNT 100
 
@@ -16,6 +16,9 @@ int main(int argc, char **argv)
     int ret;
     char error_buffer[100];
     regmatch_t pmatch[MATCH_COUNT];
+    char* string = argv[3];
+    char* subst = argv[2];
+    char new_string[strlen(string) + strlen(subst)];
 
     ret = regcomp(&regex, argv[1], REG_EXTENDED);
     if (ret)
@@ -25,21 +28,46 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    ret = regexec(&regex, argv[3], MATCH_COUNT, pmatch, 0);
+    ret = regexec(&regex, string, MATCH_COUNT, pmatch, 0);
     if (!ret)
     {
-        /*
-        printf("%d %d\n", pmatch[0].rm_so, pmatch[0].rm_eo);
-        printf("%d %d\n", pmatch[1].rm_so, pmatch[1].rm_eo);
-        printf("%d %d\n", pmatch[2].rm_so, pmatch[2].rm_eo);
-        printf("%d %d\n", pmatch[3].rm_so, pmatch[3].rm_eo);
-        */
-        fwrite(argv[3], 1, pmatch[0].rm_so, stdout);
-        printf("%s%s\n", argv[2], (char *) (argv[3] + pmatch[0].rm_eo));   
+        int new_string_len = pmatch[0].rm_so;
+        strncpy(new_string, string, pmatch[0].rm_so);
+
+        int match_count = 0;
+        for (int i = 1; pmatch[i].rm_so != -1; i++)
+        {
+            match_count++;
+        }
+        for (int i = 0; i < strlen(subst); i++)
+        {
+            if (subst[i] == '\\' && i < strlen(subst) - 1 && subst[i + 1] < '9' && subst[i + 1] > '0')
+            {
+                int match_num = subst[i + 1] - '0';
+                //printf("if %s %d %d %s %d\n", string + pmatch[match_num].rm_so, pmatch[match_num].rm_so, pmatch[match_num].rm_eo, new_string, new_string_len);
+                if (match_num > match_count)
+                {
+                    printf("Wrong match number: %d\n", match_num);
+                    return 1;
+                }
+                strncpy(new_string + new_string_len, string + pmatch[match_num].rm_so, pmatch[match_num].rm_eo - pmatch[match_num].rm_so);
+                new_string_len += pmatch[match_num].rm_eo - pmatch[match_num].rm_so;
+                i++;
+            }
+            else
+            {
+                //printf("else %c %s %d\n", subst[i], new_string, new_string_len);
+                new_string[new_string_len] = subst[i];
+                new_string_len++;
+            }
+            
+        }
+        new_string[new_string_len] = '\0';
+        printf("%s%s\n", new_string, (char *) (string + pmatch[0].rm_eo));   
     }
     else if (ret == REG_NOMATCH)
     {
-        printf("%s\n", argv[3]);
+        printf("%s\n", string);
     }
     else
     {
